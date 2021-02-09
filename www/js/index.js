45a1885d76e066c204c4054310204f34b3268148
JCapelDev <win200003@gmail.com>
2021-02-09 17:48:36 +0100

    First Commit

---
+++ www/js/index.js
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+// Wait for the deviceready event before using any of Cordova's device APIs.
+// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
+document.addEventListener('deviceready', onDeviceReady, false);
+
+var users = {
+	"pepe":"pepe123",
+	"lola":"lola123",
+};
+
+function check_login(){
+	var user = document.getElementById("username").value;
+	var pass = document.getElementById("pass").value;
+	if(users[user]==pass)
+		alert("U r In");
+	else
+		alert("Wrong");
+}
+
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+   //document.getElementById('deviceready').classList.add('ready');
+   var login_button = document.getElementById("login_button").onclick = check_login;
+}
